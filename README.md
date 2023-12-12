6210c112006bf9488fb3e374650021447eaa4933
Maschkov Konstantin <maschkovc@yandex.ru>
2023-12-12 18:45:13 +0300

    mern init

---
+++ README.md
@@ -0,0 +1,6 @@
+<!-- ijgKhLX0hI9hQWx9 -->
+<!-- tigrrr71 -->
+
+<!-- mongodb+srv://tigrrr71:<password>@cluster0.fa5llaa.mongodb.net/?retryWrites=true&w=majority -->
+
+<!-- npm run start:dev -->
---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { registerValidation, loginValidation, postCreateValidation } from "./validations.js";
+
+import checkAuth from './utils/checkAuth.js';
+import * as UserController from './controllers/UserController.js';
+import * as PostController from './controllers/PostController.js';
+
+
+
+mongoose
+	.connect('mongodb+srv://tigrrr71:ijgKhLX0hI9hQWx9@cluster0.fa5llaa.mongodb.net/blog?retryWrites=true&w=majority')
+	.then(() => { console.log('DB ok') })
+	.catch((err) => console.log("DB error", err));
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/auth/login', loginValidation, UserController.login);
+app.post('/auth/register', registerValidation, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, PostController.update);
+
+
+app.listen(4444, (err) => {
+	if (err) {
+		return console.log(err);
+	}
+	console.log('Server OK');
+});
+
+
+// npm run start:dev
+
+//1.50
